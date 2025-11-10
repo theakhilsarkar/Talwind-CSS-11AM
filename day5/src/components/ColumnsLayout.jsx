61a0d65251ad8e8f74dad62d397787308b60677d
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-11-10 12:09:05 +0530

    .

--- day5/src/components/ColumnsLayout.jsx
+++ day5/src/components/ColumnsLayout.jsx
@@ -5,7 +5,7 @@ export default function ColumnsLayout() {
         <div className='bg-gray-200 2xl:columns-4 xl:columns-3 md:columns-2 columns-1 m-3 space-y-3 space-x-3'>
 
             <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
+                <img src="https://picsum.photos/400/300/random/1" alt="" className='rounded mb-3 w-full' />
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
             </div>
 
