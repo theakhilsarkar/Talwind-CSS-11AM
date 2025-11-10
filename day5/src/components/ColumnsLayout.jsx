600858e43b782bafdd4b61d1737ef3750978e083
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-11-10 12:15:33 +0530

    ..

--- day5/src/components/ColumnsLayout.jsx
+++ day5/src/components/ColumnsLayout.jsx
@@ -4,40 +4,14 @@ export default function ColumnsLayout() {
     return (
         <div className='bg-gray-200 2xl:columns-4 xl:columns-3 md:columns-2 columns-1 m-3 space-y-3 space-x-3'>
 
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300/random/1" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
+            {
 
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
 
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
-
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
-
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
-
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
-
-            <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
-                <img src="https://picsum.photos/400/300" alt="" className='rounded mb-3 w-full' />
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
-            </div>
+                Array.from({ length: 11 }).map((_, i) => <div className='bg-sky-50 p-3 rounded break-inside-avoid'>
+                    <img src={"https://picsum.photos/300/200?random=" + i} alt="" className='rounded mb-3 w-full' />
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius nisi molestias tempora quisquam odio optio, doloribus sint? Optio, doloribus </p>
+                </div>)
+            }
 
         </div>
     )
