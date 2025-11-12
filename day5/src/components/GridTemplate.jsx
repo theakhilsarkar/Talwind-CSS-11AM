31a126d778205e13bdb1a5565e25eb65c9079bd4
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-11-12 12:06:42 +0530

    ..

--- day5/src/components/GridTemplate.jsx
+++ day5/src/components/GridTemplate.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default function GridTemplate() {
     return (
         <div className='container mx-auto grid grid-cols-4'>
-            {/* {
-                Array.from({ length: 9 }).map((_, i) => {
+            {
+                Array.from({ length: 19 }).map((_, i) => {
                     const color = colorList[i % colorList.length];
                     return <div key={i} className={color + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
                 })
-            } */}
+            }
             {/* <div className='col-span-3 grid grid-cols-subgrid'>
                 <div key="11" className={"bg-emerald-200" + ' col-start-2 m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
             </div> */}
 
 
-            <div className={"bg-emerald-200" + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
+            {/* <div className={"bg-emerald-200" + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
 
             <div className={"bg-emerald-200" + '  m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
 
@@ -32,7 +32,7 @@ export default function GridTemplate() {
 
             <div className={"bg-emerald-200" + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
 
-            <div className={"bg-emerald-200 col-span-4" + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div>
+            <div className={"bg-emerald-200 col-span-4" + ' m-4 p-4 rounded-md'}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae placeat saepe cum possimus earum ipsa voluptatem esse officia distinctio voluptatibus necessitatibus, accusantium vitae, delectus sapiente vel excepturi fugiat ullam nulla.</div> */}
 
         </div>
 
